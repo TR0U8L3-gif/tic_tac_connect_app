44abfaa868572fdd929fcd0ac6374fc657968643
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2024-01-18 13:03:42 +0100

    on_boarding guard using bloc

--- lib/config/routes/guard/on_boarding_guard.dart
+++ lib/config/routes/guard/on_boarding_guard.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:tic_tac_connect_app/core/common/domain/usecase/use_case.dart';
 import 'package:tic_tac_connect_app/core/services/dependency_injection/injection_container.dart';
 import 'package:tic_tac_connect_app/src/dashboard/presentation/pages/dashboard_page.dart';
-import 'package:tic_tac_connect_app/src/on_boarding/domain/use_cases/check_if_first_timer.dart';
+import 'package:tic_tac_connect_app/src/on_boarding/presentation/manager/on_boarding_bloc.dart';
 
 class OnBoardingGuard extends AutoRouteGuard {
   @override
@@ -11,18 +13,35 @@ class OnBoardingGuard extends AutoRouteGuard {
     NavigationResolver resolver,
     StackRouter router,
   ) async {
-    final result = await sl<CheckIfFirstTimer>()(NoParams());
-    result.fold(
-      (l) => resolver.next(),
-      (r) {
-        if(r){
-          resolver.next();
-        } else {
-          router.replaceNamed(DashboardPage.routeName);
+    final bloc = sl<OnBoardingBloc>();
+
+    StreamSubscription<OnBoardingState>? streamSubscription;
+    streamSubscription = bloc.stream.listen(
+      (state) {
+        print(state);
+        if (state is OnBoardingStatusState) {
+          if (state.isFirstTimer) {
+            resolver.next();
+          } else {
+            router.replaceNamed(DashboardPage.routeName);
+          }
+
+          FlutterNativeSplash.remove();
+          streamSubscription?.cancel();
         }
+      },
+      onError: (Object e) {
+        debugPrint('onError $e');
+        resolver.next();
+        streamSubscription?.cancel();
+      },
+      onDone: () {
+        debugPrint('onDone');
       },
     );
 
-    FlutterNativeSplash.remove();
+    bloc.add(
+      const ShowOnBoardingEvent(checkIfFirstTimer: true),
+    );
   }
 }
